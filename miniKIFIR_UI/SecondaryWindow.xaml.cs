d8483634fbb2d1a3964c408892b0d12adb8acc12
Benedek Kónya <benedekkonya@gmail.com>
2024-01-25 17:25:56 +0100

    Extra feladat megvalósítva, utolsó ellenörzések elkészítve

--- miniKIFIR_UI/SecondaryWindow.xaml.cs
+++ miniKIFIR_UI/SecondaryWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Text.RegularExpressions;
 using System.ComponentModel;
 using System.Globalization;
 using System.Windows.Threading;
+using System.Linq;
 
 namespace miniKIFIR_UI
 {
@@ -24,11 +25,6 @@ namespace miniKIFIR_UI
         bool uzemMod;
         private DateTime convertedDate;
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
         public MainWindow(Adatok ujdiak, bool mod) : this()
         {
             this.felvetelizoAdatai = ujdiak;
@@ -37,29 +33,23 @@ namespace miniKIFIR_UI
             {
                 txtOmAzon.IsEnabled = true;
             }
-            else { txtOmAzon.IsEnabled = false;}
+            else { txtOmAzon.IsEnabled = false; }
         }
 
-        private void btnAppExit_Click(object s, RoutedEventArgs e)
+        public MainWindow()
         {
-            WindowManager();
-        }
-        private void btnAppMinimize_Click(object s, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
+            InitializeComponent();
         }
 
-        private void NumberValidation(object s, TextCompositionEventArgs e)
+        //Ablak bezárás, erőforrások felszabadítása
+        private void WindowManager()
         {
-            Regex regex = new Regex("[^0-9]+");
-            e.Handled = regex.IsMatch(e.Text);
-        }
-        private void TextValidation(object s, TextCompositionEventArgs e)
-        {
-            Regex regex = new Regex("[^a-zA-Z\\P{IsBasicLatin}]+");
-            e.Handled = regex.IsMatch(e.Text);
+            Window UI_Window = Window.GetWindow(this);
+            UI_Window.Resources.Clear();
+            UI_Window.Close();
         }
 
+        //Ablak mozgatása
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -68,6 +58,87 @@ namespace miniKIFIR_UI
             }
         }
 
+        //Ablakbezárás megerősítéssel
+        private void btnAppExit_Click(object s, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Biztosan ki szeretne lépni?\nFigyelem ez a művelet megszakítja az adatfelvételt!", "Figyelem", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                WindowManager();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        //Abalak minimalizálása
+        private void btnAppMinimize_Click(object s, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        //Szám ellenörzés, ezt használja az OM Azonosító, Magyar, Matek pont mezők.
+        private void NumberValidation(object s, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        //Szöveg ellenörzés, ezt használja a Név mező
+        private void TextValidation(object s, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^a-zA-Z\\P{IsBasicLatin}]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        //Checkbox ellenörzés, magyar illetve matek mezők be/kikapcsolása.
+        private void cbPontRögzit_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbPontRögzit.IsChecked == true)
+            {
+                txtMatekE.IsEnabled = true;
+                txtMagyarE.IsEnabled = true;
+            }
+            else
+            {
+                txtMatekE.IsEnabled = false;
+                txtMagyarE.IsEnabled = false;
+            }
+        }
+
+        //Név ellenörzés, 2 szó, felesleges space-ek kiszűrése
+        private void txtNev_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string[] nev = { String.Empty };
+
+            if (txtNev.Text.Contains(' '))
+            {
+                nev = txtNev.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (nev.Length >= 2)
+                {
+                    for (int i = 0; i < nev.Length; i++)
+                    {
+                        if (!char.IsUpper(nev[i][0]))
+                        {
+                            MessageBox.Show("Nem megfelelő név!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                            txtNev.Clear();
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("A névnek legalább 2 szóból kell állnia (Vezetéknév, Keresztnév).", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("A névnek muszáj legalább egy 'SPACE'-t tartalmaznia", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        //Dátum ellenörzés a maszkolt textboxból.
         private void maskedTextBox1_LostFocus(object sender, RoutedEventArgs e)
         {
             string dateString = maskedTextBox1.Text;
@@ -113,23 +184,55 @@ namespace miniKIFIR_UI
             }
         }
 
+        //Rögzít gomb, adat átadás, illetve adatok ellenörzése.
         private void btnRogzit_Click(object sender, RoutedEventArgs e)
         {
-            if (txtMatekE.Text == "" || txtMagyarE.Text == "" || txtNev.Text == "" || txtOmAzon.Text == "" || txtErtesitesi.Text == "" || txtEmail.Text == "")
+            if (cbPontRögzit.IsChecked == true)
+            {
+                if (txtMatekE.Text == "" || txtMagyarE.Text == "" || txtNev.Text == "" || txtOmAzon.Text == "" || txtErtesitesi.Text == "" || txtEmail.Text == "")
+                {
+                    MessageBox.Show("Nem lehet üres mező!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else if (int.Parse(txtMagyarE.Text) > 50 || 0 > int.Parse(txtMagyarE.Text))
+                {
+                    MessageBox.Show("Nem lehet ennyi pontja magyarból a felvételizőnek!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                    txtMagyarE.Clear();
+                    txtMagyarE.Focus();
+                }
+                else if (txtEmail.Text.Count(x => x == '@') != 1)
+                {
+                    MessageBox.Show($"Az email cím mező több mint egy '@' jelet tartalmaz, nem megfelelő email!\n '@' Jelek  száma:{txtEmail.Text.Count(x => x == '@')}",
+                        "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else if (int.Parse(txtMatekE.Text) > 50 || 0 > int.Parse(txtMatekE.Text))
+                {
+                    MessageBox.Show("Nem lehet ennyi pontja matematikából a felvételizőnek!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                    txtMatekE.Clear();
+                    txtMatekE.Focus();
+                }
+                else
+                {
+                    this.felvetelizoAdatai.OM_Azonosito = txtOmAzon.Text;
+                    this.felvetelizoAdatai.Neve = txtNev.Text;
+                    this.felvetelizoAdatai.Email = txtEmail.Text;
+                    this.felvetelizoAdatai.SzuletesiDatum = convertedDate;
+                    this.felvetelizoAdatai.ErtesitesiCime = txtErtesitesi.Text;
+                    this.felvetelizoAdatai.Matematika = int.Parse(txtMatekE.Text);
+                    this.felvetelizoAdatai.Magyar = int.Parse(txtMagyarE.Text);
+                    MessageBox.Show("Az adatok sikeresen rögzítve lettek!", "Sikeres rögzítés", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.DialogResult = true;
+
+                    WindowManager();
+                }
+            }
+            else if (txtNev.Text == "" || txtOmAzon.Text == "" || txtErtesitesi.Text == "" || txtEmail.Text == "") 
             {
                 MessageBox.Show("Nem lehet üres mező!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            else if (int.Parse(txtMagyarE.Text) > 50 || 0 > int.Parse(txtMagyarE.Text))
+            else if (txtEmail.Text.Count(x => x == '@') != 1)
             {
-                MessageBox.Show("Nem lehet ennyi pontja magyarból a felvételizőnek!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
-                txtMagyarE.Clear();
-                txtMagyarE.Focus();
-            }
-            else if (int.Parse(txtMatekE.Text) > 50 || 0 > int.Parse(txtMatekE.Text))
-            {
-                MessageBox.Show("Nem lehet ennyi pontja matematikából a felvételizőnek!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
-                txtMatekE.Clear();
-                txtMatekE.Focus();
+                MessageBox.Show($"Az email cím mező több mint egy '@' jelet tartalmaz, nem megfelelő email!\n '@' Jelek  száma:{txtEmail.Text.Count(x => x == '@')}",
+                    "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             else
             {
@@ -138,20 +241,11 @@ namespace miniKIFIR_UI
                 this.felvetelizoAdatai.Email = txtEmail.Text;
                 this.felvetelizoAdatai.SzuletesiDatum = convertedDate;
                 this.felvetelizoAdatai.ErtesitesiCime = txtErtesitesi.Text;
-                this.felvetelizoAdatai.Matematika = int.Parse(txtMatekE.Text);
-                this.felvetelizoAdatai.Magyar = int.Parse(txtMagyarE.Text);
                 MessageBox.Show("Az adatok sikeresen rögzítve lettek!", "Sikeres rögzítés", MessageBoxButton.OK, MessageBoxImage.Information);
                 this.DialogResult = true;
 
                 WindowManager();
             }
-        }
-
-        private void WindowManager()
-        {
-            Window UI_Window = Window.GetWindow(this);
-            UI_Window.Resources.Clear();
-            UI_Window.Close();
         }
     }
 }
